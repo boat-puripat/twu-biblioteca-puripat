7351df15aeacb23e90c5dbbea0b5ba6269d2b955
Puripat Arayasirikul <puripat.arayasirikul@Puripats-MacBook-Pro-2.local>
2021-05-06 13:18:15 +0700

    :sparkles: view a welcome message

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,6 +3,11 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        printWelcome();
+    }
+
+    public static void printWelcome() {
+        String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        System.out.println(welcomeMessage);
     }
 }
