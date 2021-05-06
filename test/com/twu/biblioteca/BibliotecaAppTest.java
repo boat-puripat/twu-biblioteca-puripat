b3e015f9863e0798f4b42f70e1ff3c3518a1538c
Puripat Arayasirikul <puripat.arayasirikul@Puripats-MacBook-Pro-2.local>
2021-05-06 13:48:10 +0700

    :white_check_mark: add test list all books only name

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -3,8 +3,10 @@ package com.twu.biblioteca;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import java.awt.print.Book;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 public class BibliotecaAppTest {
     @Test
@@ -14,6 +16,21 @@ public class BibliotecaAppTest {
         System.setOut(outStream);
         BibliotecaApp.printWelcome();
         String expectedValue = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!\n";
+        assertEquals(expectedValue, outputContent.toString());
+    }
+
+    @Test
+    public void shouldListAllBooks() {
+        ByteArrayOutputStream outputContent = new ByteArrayOutputStream();
+        PrintStream outStream = new PrintStream(outputContent);
+        System.setOut(outStream);
+        ArrayList<Book> books = new ArrayList<>() {{
+            add(new Book("Hello"));
+            add(new Book("World"));
+        }};
+
+        BibliotecaApp.listAllBooks(books);
+        String expectedValue = "Hello\nWorld\n";
         assertEquals(expectedValue, outputContent.toString());
     }
 }
