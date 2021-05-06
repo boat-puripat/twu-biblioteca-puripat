1dac9d9cc08235f50710dae49056774cf25787e1
Puripat Arayasirikul <puripat.arayasirikul@Puripats-MacBook-Pro-2.local>
2021-05-06 14:00:00 +0700

    :sparkles: list all books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,13 +1,26 @@
 package com.twu.biblioteca;
 
+import java.util.ArrayList;
+
 public class BibliotecaApp {
+    private static ArrayList<Book> books = new ArrayList<>() {{
+        add(new Book("Hello"));
+        add(new Book("World"));
+    }};
 
     public static void main(String[] args) {
         printWelcome();
+        listAllBooks(books);
     }
 
     public static void printWelcome() {
         String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
         System.out.println(welcomeMessage);
+    }
+
+    public static void listAllBooks(ArrayList<Book> books) {
+        for (Book book: books) {
+            System.out.println(book.getName());
+        }
     }
 }
