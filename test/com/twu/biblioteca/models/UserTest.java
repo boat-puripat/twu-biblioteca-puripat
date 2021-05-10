90d78723767e850bbe5961df16a1727ee7ac0864
Puripat Arayasirikul <puripat.arayasirikul@Puripats-MacBook-Pro-2.local>
2021-05-10 16:19:21 +0700

    :white_check_mark: test method equal for compare user

---
+++ test/com/twu/biblioteca/models/UserTest.java
@@ -0,0 +1,49 @@
+package com.twu.biblioteca.models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+
+public class UserTest {
+    User testUser;
+
+    @Before
+    public void setup() {
+        testUser = new User("username", "password");
+    }
+
+    @Test
+    public void testEqualWithSameReference() {
+        boolean actualValue = testUser.equals(testUser);
+        assertTrue(actualValue);
+    }
+
+    @Test
+    public void testEqualWithNull() {
+        boolean actualValue = testUser.equals(null);
+        assertFalse(actualValue);
+    }
+
+    @Test
+    public void testEqualWithDifferenceClass() {
+        String user = "user";
+        boolean actualValue = testUser.equals(user);
+        assertFalse(actualValue);
+    }
+
+    @Test
+    public void testEqualWithSameLibraryNumberAndPassword() {
+        User testUser2 = new User("username", "password");
+        boolean actualValue = testUser.equals(testUser2);
+        assertTrue(actualValue);
+    }
+
+    @Test
+    public void testEqualWithDifferenceLibraryNumberAndPassword() {
+        User testUser2 = new User("username", "123456");
+        boolean actualValue = testUser.equals(testUser2);
+        assertFalse(actualValue);
+    }
+}
