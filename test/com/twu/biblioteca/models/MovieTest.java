9a2272949d09fd279c2b9bd0ab862b9adf829171
Puripat Arayasirikul <puripat.arayasirikul@Puripats-MacBook-Pro-2.local>
2021-05-10 13:41:45 +0700

    :white_check_mark: test movie get name & to string

---
+++ test/com/twu/biblioteca/models/MovieTest.java
@@ -0,0 +1,29 @@
+package com.twu.biblioteca.models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MovieTest {
+    Movie testMovie;
+
+    @Before
+    public void setup() {
+        testMovie = new Movie("name", "director", 2021, 10);
+    }
+
+    @Test
+    public void testGetName() {
+        String expectValue = "name";
+        String actualValue = testMovie.getName();
+        assertEquals(expectValue, actualValue);
+    }
+
+    @Test
+    public void testToString() {
+        String expectValue = "name\t2021\tdirector\t10";
+        String actualValue = testMovie.toString();
+        assertEquals(expectValue, actualValue);
+    }
+}
